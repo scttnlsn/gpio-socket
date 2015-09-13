18b8347fb5677f5591ae13bc11c35d0b76616b97
Scott Nelson <scott@scottnelson.co>
2015-09-13 14:31:29 -0400

    v0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject gpio-socket "0.1.0-SNAPSHOT"
+(defproject gpio-socket "0.1.0"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.48"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
